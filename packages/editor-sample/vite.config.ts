7b38b62bee11224dd98eff4af6e21710ccdd9154
miloszbranewicz <m.branewicz@wp.pl>
2024-08-04 18:06:51 +0200

    build: update rollup options for template generation

--- packages/editor-sample/vite.config.ts
+++ packages/editor-sample/vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   base: '/email-builder-js/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'template-generator.js',
+        chunkFileNames: 'template-generator-[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 });
